984b0a4486fc6d4b57b77a93ce92a81e37356be2
lipeng <734991033@qq.com>
2015-11-16 10:30:16 +0800

    Signed-off-by: lipeng <734991033@qq.com>

--- md.go
+++ md.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"os"
 	"html/template"
 	"log"
 	"net/http"
 )
 var (
-	view_Dir="./views"
+	root=""
+	view_Dir="/views"
 )
 var 引擎=template.New("引擎")
 func initTmpl(){
-	引擎, _ = template.ParseFiles(view_Dir+"/"+"header.tmpl",view_Dir+"/"+ "writingspace.tmpl", view_Dir+"/"+"footer.tmpl")
+	引擎, _ = template.ParseFiles(root+"/"+view_Dir+"/"+"header.tmpl",root+"/"+view_Dir+"/"+ "writingspace.tmpl",root+"/"+ view_Dir+"/"+"footer.tmpl")
 }
 func writingspaceHandler(w http.ResponseWriter,r *http.Request){
 	switch r.Method{
@@ -20,13 +22,24 @@ func writingspaceHandler(w http.ResponseWriter,r *http.Request){
 				log.Fatalln(err.Error())
 			}
 		case "POST":
-					
+			
 		}
 }
+func staticDirHandler(mux *http.ServeMux,prefix string,staticDir string,flags int){
+	mux.HandleFunc(prefix,
+		func(w http.ResponseWriter,r *http.Request){
+			log.Println(r.URL.Path)
+			file:=root+r.URL.Path
+			log.Println(file)
+			http.ServeFile(w,r,file)
+		})
+}
 func main(){
+	//检查根目录
+	root,_=os.Getwd()
 	initTmpl()
 	var mux=http.NewServeMux()
-	
+	staticDirHandler(mux,"/assets/",root+"/assets",0)
 	mux.HandleFunc("/writingspace",writingspaceHandler)
 	err:=http.ListenAndServe(":8090",mux)
 	log.Println("http.ListenAndServe(:8090)")
